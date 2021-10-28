cba5d6be44eeaeb97a8a7e66c9f7f883853c227d
sparsh0602 <75162752+sparsh0602@users.noreply.github.com>
2021-10-29 02:06:15 +0530

    Update game mania.js

--- WEBSITE CODE/game mania.js
+++ WEBSITE CODE/game mania.js
@@ -13,3 +13,126 @@ function SlideShow() {
   setTimeout(SlideShow, 2000); // Change image every 2 seconds
 } 
 
+
+
+ document.getElementsByTagName('select')[0].addEventListener('change',function(){ 
+  var val = document.getElementsByTagName('select')[0].value; 
+ 
+  
+  if(val==="SNAKE MANIA")
+  {
+  let a=document.querySelector("#snake1");
+  let b=document.querySelector("#snake2");
+  let c=document.querySelector("#snake3");
+  let d=document.querySelector("#snake4");
+
+   a.style.display="block";
+   b.style.display="none";
+   c.style.display="none";
+   d.style.display="none";
+    
+    let a1=document.querySelector("#DM");
+    let b1=document.querySelector("#SM");
+    let c1=document.querySelector("#TT");
+    let d1=document.querySelector("#FI");
+    
+    a1.style.display="none";
+    b1.style.display="block";
+    c1.style.display="none";
+    d1.style.display="none";
+    
+  }
+
+  if(val==="TIC TAC TOE")
+  {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="none";
+   c.style.display="block";
+   d.style.display="none";
+    
+    let a1=document.querySelector("#DM");
+    let b1=document.querySelector("#SM");
+    let c1=document.querySelector("#TT");
+    let d1=document.querySelector("#FI");
+    
+    a1.style.display="none";
+    b1.style.display="none";
+    c1.style.display="block";
+    d1.style.display="none";
+     
+    }
+
+    if(val==="DINOSAUR MANIA")
+    {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="block";
+   c.style.display="none";
+   d.style.display="none";
+      
+    let a1=document.querySelector("#DM");
+    let b1=document.querySelector("#SM");
+    let c1=document.querySelector("#TT");
+    let d1=document.querySelector("#FI");
+    
+    a1.style.display="block";
+    b1.style.display="none";
+    c1.style.display="none";
+    d1.style.display="none";
+    }
+   
+    if(val==="FLIP IT")
+    {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="none";
+   c.style.display="none";
+   d.style.display="block";
+      
+    let a1=document.querySelector("#DM");
+    let b1=document.querySelector("#SM");
+    let c1=document.querySelector("#TT");
+    let d1=document.querySelector("#FI");
+    
+    a1.style.display="none";
+    b1.style.display="none";
+    c1.style.display="none";
+    d1.style.display="block";
+    }
+   
+    if(val==="#")
+    {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="block";
+   b.style.display="block";
+   c.style.display="block";
+   d.style.display="block";
+      
+    let a1=document.querySelector("#DM");
+    let b1=document.querySelector("#SM");
+    let c1=document.querySelector("#TT");
+    let d1=document.querySelector("#FI");
+    
+    a1.style.display="block";
+    b1.style.display="block";
+    c1.style.display="block";
+    d1.style.display="block";
+    }
+ }); 
